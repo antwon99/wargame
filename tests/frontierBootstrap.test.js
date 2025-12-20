830a801c6ef14a8a107ebbfc7999b1d5306921e7
antwon99 <anthonyhoward@live.ca>
2025-12-20 10:32:18 -0500

    fix: lazily load imperial mandates in browser bootstrap

--- tests/frontierBootstrap.test.js
+++ tests/frontierBootstrap.test.js
@@ -20,7 +20,7 @@ async function run() {
     Game.playSound = () => {};
     Game.showTileCallout = () => {};
     Game.hideTileCallout = () => {};
-    Game.bootstrapNewWorld();
+    await Game.bootstrapNewWorld();
 
     const mandateState = ImperialMandates.getKingState().mandates.destroy_first_rebel_camp;
     assert.strictEqual(mandateState.status, ImperialMandates.MandateStatus.ACTIVE, 'Frontier Sweep should start active.');
